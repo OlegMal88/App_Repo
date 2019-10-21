1cdb50208b9c1032611b8728a8c0ed727208443d
Andrey Sokolov <artas90@gmail.com>
2019-10-21 14:42:21 +0300

    Move serveice to separate  module

--- src/core/state/counter/store-feature.module.ts
+++ src/core/state/counter/store-feature.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {StoreModule} from '@ngrx/store';
+import {CommonModule} from '@angular/common';
 import {EffectsModule} from '@ngrx/effects';
 import {CounterEffects} from '@state/counter/effects';
-import {ApiModule} from '@api/api.module';
 import * as CounterState from './state';
 import {reducer} from './reducer';
+import {ApiModule} from '@api/api.module';
 
 @NgModule({
   imports: [
