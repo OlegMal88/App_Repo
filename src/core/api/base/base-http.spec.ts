7a4384879940cc01fa4ab386a61710f8fef8c743
Oleh_Malyshev <olegmmm33@gmail.com>
2019-10-21 12:00:51 +0300

    Commit

--- src/core/api/base/base-http.spec.ts
+++ src/core/api/base/base-http.spec.ts
@@ -1,26 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-import { BaseHttpService } from './base-http.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { CounterModel } from '@api/models/counter.model';
+import {TestBed} from '@angular/core/testing';
+import {BaseHttpService} from './base-http.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {CounterModel} from '@api/models/counter.model';
+import {TestScheduler} from 'rxjs/testing';
+import {HttpClient} from '@angular/common/http';
+import {of} from 'rxjs';
 
 fdescribe('BaseHttpService', () => {
   let service: BaseHttpService;
-  let httpMock: HttpTestingController;
+  let httpMock: any;
+  let scheduler: TestScheduler;
 
   const COUNTER_ERROR_RESPONSE_MOCK: any = 'ERROR';
 
   beforeEach(() => {
+
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual)
+        .toEqual(expected);
+    });
+
+    httpMock = {
+      get: jasmine.createSpy('get')
+        .and
+        .returnValue(scheduler.createHotObservable(
+          '-#',
+          {},
+          COUNTER_ERROR_RESPONSE_MOCK
+        ))
+    };
+
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [BaseHttpService],
+      providers: [
+        {
+          provide: HttpClient,
+          useFactory: () => httpMock
+        },
+        BaseHttpService],
     });
 
     service = TestBed.get(BaseHttpService);
-    httpMock = TestBed.get(HttpTestingController);
   });
 
   describe('getData', () => {
-    it('should return an Observable<Counter[]>', () => {
+    it('should check http,get method call', () => {
+      spyOn((service as any), 'handleError')
+        .and.returnValue(of(COUNTER_ERROR_RESPONSE_MOCK));
+
+      scheduler.expectObservable(service.getData())
+        .toBe('-(a|)', {
+          a: COUNTER_ERROR_RESPONSE_MOCK
+        });
+
+      scheduler.flush();
+
+      expect((service as any).handleError)
+        .toHaveBeenCalledWith(COUNTER_ERROR_RESPONSE_MOCK);
+    });
+    xit('should return an Observable<Counter[]>', () => {
       const counters: any[] = [new CounterModel(1, 100)];
 
       service.getData().subscribe(countersData => {
