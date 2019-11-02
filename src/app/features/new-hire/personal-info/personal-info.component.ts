eb18ee73f928c63f9d9f896bb4e49dbb26e624cb
oscaryald <oscaryald@gmail.com>
2019-11-02 12:28:10 +0200

    add new form

--- src/app/features/new-hire/personal-info/personal-info.component.ts
+++ src/app/features/new-hire/personal-info/personal-info.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { DynamicFormComponent } from '@shared/components/dynamic-form/dynamic-form.component';
+import { personalInfoFormConfig } from '@features/new-hire/personal-info/personal-info-config';
+import { FieldConfig } from '@shared/interfaces/field.interface';
 
 @Component({
   selector: 'app-personal-info',
@@ -7,44 +10,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./personal-info.component.scss']
 })
 export class PersonalInfoComponent implements OnInit {
-  personalInfoForm: FormGroup;
-  tempMockStates = [
-    'Washington',
-    'Taxes',
-    'Florida',
-  ];
-  MIN_LENTH = 4;
-
+  @ViewChild(DynamicFormComponent, {static: false}) personalInfoForm: DynamicFormComponent;
+  personalInfoFormConfig: FieldConfig[];
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
-    this.personalInfoForm = this.fb.group({
-      firstName: ['', Validators.required],
-      middleInitial: [''],
-      lastName: ['', [Validators.required]],
-      otherLastName: [''],
-      ssn: ['', [Validators.required]],
-      dateOfBirth: ['', [Validators.required]],
-      streetAdress: ['', [Validators.required]],
-      city: ['', [Validators.required]],
-      state: ['', [Validators.required]],
-      zipCode: ['', [Validators.required, Validators.minLength(this.MIN_LENTH)]],
-    });
+    this.personalInfoFormConfig = personalInfoFormConfig
+    // TODO: to choose better solution
+    
+    // this.personalInfoForm = this.fb.group({
+    //   firstName: ['', Validators.required],
+    //   middleInitial: [''],
+    //   lastName: ['', [Validators.required]],
+    //   otherLastName: [''],
+    //   ssn: ['', [Validators.required]],
+    //   dateOfBirth: ['', [Validators.required]],
+    //   streetAdress: ['', [Validators.required]],
+    //   city: ['', [Validators.required]],
+    //   state: ['', [Validators.required]],
+    //   zipCode: ['', [Validators.required, Validators.minLength(this.MIN_LENTH)]],
+    // });
   }
 
-  isInvalidInput(controlName: string) {
-    const control = this.personalInfoForm.get(controlName);
-    if (!control) {
-      return;
-    }
-    return control.touched && control.invalid;
-  }
+  // isInvalidInput(controlName: string) {
+  //   const control = this.personalInfoForm.get(controlName);
+  //   if (!control) {
+  //     return;
+  //   }
+  //   return control.touched && control.invalid;
+  // }
 
-  save() {
-    if (this.personalInfoForm.invalid) {
-      return;
-    }
-    console.log(this.personalInfoForm.value);
+  save(value) {
+    console.log(value);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,44 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { DataService } from '@services/mockServer/data.service';
 import { ErrorMessagesComponent } from './components/error-messages/error-messages.component';
+import { DynamicFormComponent } from '@shared/components/dynamic-form/dynamic-form.component';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { InputComponent } from '@shared/components/input/input.component';
+import { SelectComponent } from '@shared/components/select/select.component';
+import { DynamicFieldDirective } from '@shared/directives/dynamic-field/dynamic-field.directive';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   providers: [
     DataService,
   ],
-  declarations: [ErrorMessagesComponent],
-  exports: [ErrorMessagesComponent]
+  declarations: [
+    ErrorMessagesComponent,
+    DynamicFormComponent,
+    ButtonComponent,
+    InputComponent,
+    SelectComponent,
+    DynamicFieldDirective,
+  ],
+  exports: [
+    ErrorMessagesComponent,
+    DynamicFormComponent,
+    DynamicFieldDirective,
+  ],
+  entryComponents: [
+    InputComponent,
+    ButtonComponent,
+    SelectComponent,
+  ]
 })
 class SharedModule {
 }
